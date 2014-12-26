663869de7209b15ecc2f4519df1cf074a981f48f
Chong-Lim Kim <clkim@ieee.org>
2014-12-26 16:33:36 -0500

    Initial commit starting from Android Studio's Blank Activity template
    Initial adding of Toolbar to replace ActionBar, following
     http://android-developers.blogspot.com/2014/10/appcompat-v21-material-design-for-pre.html
    in "Toolbar Widget" section, but adapted NOT to use AppCompat since we do need min API 21.
    
    Changes:
      v21 styles - noactionbar theme
      layout - add toolbar element with the suggested layout in this section
             - remove paddings from RelativeLayout root and add to TextView
             - position TextView below Toolbar
      call setSupportActionBar() in onCreate()
    
    Not sure toolbar is complete, but seems to hook in ok to the 1 menu item; will do for now.

---
+++ app/src/main/java/com/jinmobi/yole/BLeDevicesActivity.java
@@ -0,0 +1,43 @@
+package com.jinmobi.yole;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+
+public class BLeDevicesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ble_devices);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_ble_devices, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
